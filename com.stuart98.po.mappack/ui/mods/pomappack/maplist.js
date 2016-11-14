375567ee026a057234682a7e26adc0de90db3509
Stuart Hepworth <robostuart@gmail.com>
2016-11-13 23:35:00 -0700

    Published the map pack.

---
+++ com.stuart98.po.mappack/ui/mods/pomappack/maplist.js
@@ -0,0 +1,8 @@
+﻿cShareSystems.load_pas("superouman", [
+	"coui://ui/mods/pomappack/systems/boading_multiplanet.pas",
+	"coui://ui/mods/pomappack/systems/closair.pas",
+	"coui://ui/mods/pomappack/systems/meso.pas",
+	"coui://ui/mods/pomappack/systems/shipyards.pas",
+	"coui://ui/mods/pomappack/systems/pacific_x.pas",
+	"coui://ui/mods/pomappack/systems/theclassybarren.pas"
+]);
