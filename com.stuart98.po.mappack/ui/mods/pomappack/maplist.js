ff0913c828986f0f2431f73a5345db4cbe8468fd
Stuart Hepworth <robostuart@gmail.com>
2016-11-13 23:49:48 -0700

    damnit

--- com.stuart98.po.mappack/ui/mods/pomappack/maplist.js
+++ com.stuart98.po.mappack/ui/mods/pomappack/maplist.js
@@ -1,4 +1,4 @@
-﻿cShareSystems.load_pas("superouman", [
+﻿cShareSystems.load_pas("Planetary Obliteration", [
 	"coui://ui/mods/pomappack/systems/boading_multiplanet.pas",
 	"coui://ui/mods/pomappack/systems/closair.pas",
 	"coui://ui/mods/pomappack/systems/meso.pas",
